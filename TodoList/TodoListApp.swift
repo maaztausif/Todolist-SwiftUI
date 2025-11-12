01ef39815c75e678a3dc01b4db988226e0c3ee9a
maaztausif <maaztausif48@gmail.com>
2025-11-12 02:46:02 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Maaz Bin Tausif on 12.11.25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoListTests/TodoListTests.swift
@@ -0,0 +1,16 @@
+//
+//  TodoListTests.swift
+//  TodoListTests
+//
+//  Created by Maaz Bin Tausif on 12.11.25.
+//
+
+import Testing
+
+struct TodoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
